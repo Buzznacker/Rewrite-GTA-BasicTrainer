23d1129c23ea71e0c12e4490656a207964981d34
Buzznacker <getdrilledmcgc@gmail.com>
2019-03-09 04:05:04 -0500

    Added layout for Module structure

---
+++ TrainerLib/Module/Impl/RunModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainerLib.Module.Impl
+{
+    /// <summary>
+    /// This type of module is a non toggleable module, it is a simple module which you only run once. For example "Fix Car"
+    /// </summary>
+    public class RunModule
+    {
+    }
+}
---
+++ TrainerLib/Module/Impl/ToggleAbleModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainerLib.Module.Impl
+{
+    /// <summary>
+    /// This type of module toggleable therefor you may toggle it off and on.
+    /// </summary>
+    public class ToggleAbleModule
+    {
+    }
+}
